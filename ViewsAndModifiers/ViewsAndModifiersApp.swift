f2018bf9e4a5003347f87210327d54625ebd2273
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-26 20:40:26 +0200

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Vahtee Boo on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
